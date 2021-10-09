95609f60e44e00774dc9a2960fb3844612ede5fe
Alexander Casanova <alexcasanova2008@gmail.com>
2021-10-09 08:58:14 -0400

    added pets to person struct

--- 8-methods/methods.go
+++ 8-methods/methods.go
@@ -5,10 +5,16 @@ import "fmt"
 type person struct {
 	name string
 	age  int
+	pets []pet
+}
+
+type pet struct {
+	name string
+	age  int
 }
 
 func (p person) SayHello() {
-	fmt.Printf("Hello, my name is %s and I am %d years of age", p.name, p.age)
+	fmt.Printf("Hello, my name is %s and I am %d years of age\n", p.name, p.age)
 }
 
 func (p *person) ChangeMyName(newName string) {
@@ -19,4 +25,8 @@ func main() {
 	s.SayHello()
 	s.ChangeMyName("Alex")
 	s.SayHello()
+
+	foo := pet{name: "foo", age: 1}
+	s.pets = append(s.pets, foo)
+	fmt.Println("Pets", s.pets)
 }
